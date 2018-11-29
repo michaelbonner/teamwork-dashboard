ab38b2eaec7043089cf858bc3dec03a819aad752
Michael Bonner <michaelwbonner@gmail.com>
2018-11-29 11:07:14 -0700

    teams table and model

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $teams = Team::all();
+        return view(
+            'welcome',
+            compact('teams')
+        );
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,45 +11,18 @@
                     {{ config('app.name', 'Laravel') }}
                 </h1>
                 <div class="flex">
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg m-2">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2 tracking-wide">The Coldest Sunset</div>
-                            <p class="text-grey-darker text-base">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                            </p>
+                    @foreach($teams as $team)
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg m-2 bg-white">
+                            <div class="px-6 py-4">
+                                <div class="font-bold text-xl mb-2 tracking-wide">{{ $team->name }}</div>
+                            </div>
+                            <div class="px-6 py-4">
+                                <span class="inline-block bg-grey-lighter rounded-full my-1 px-3 py-1 text-sm font-semibold text-grey-darker mr-2">photography</span>
+                                <span class="inline-block bg-grey-lighter rounded-full my-1 px-3 py-1 text-sm font-semibold text-grey-darker mr-2">travel</span>
+                                <span class="inline-block bg-grey-lighter rounded-full my-1 px-3 py-1 text-sm font-semibold text-grey-darker">winter</span>
+                            </div>
                         </div>
-                        <div class="px-6 py-4">
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
-                        </div>
-                    </div>
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg m-2">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2 tracking-wide">The Coldest Sunset</div>
-                            <p class="text-grey-darker text-base">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                            </p>
-                        </div>
-                        <div class="px-6 py-4">
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
-                        </div>
-                    </div>
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg m-2">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2 tracking-wide">The Coldest Sunset</div>
-                            <p class="text-grey-darker text-base">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                            </p>
-                        </div>
-                        <div class="px-6 py-4">
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-                            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
